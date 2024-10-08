22eb3058a857d61b3fcf2c9e5b22f1bb1a13ae69
redxffy <150579399+redxffy@users.noreply.github.com>
2024-10-08 10:59:38 +0530

    Update 176.js

--- js-coding-questions/176.js
+++ js-coding-questions/176.js
@@ -4,7 +4,7 @@ function undefinedtoNull(arg){
   //in js arrays and functions consider as an object here insted of that thing by means 
   // so non object structure will be came here the thing instead 
   // 
-  if(type of arg!=="object" || arg===""){
+  if(typeof arg!=="object" || arg==="null"){
     return arg ?? null;
     // here the retirn statement arh snd that thing non objects shpuld return null when its left handed when its was right handed null or undefined
     
