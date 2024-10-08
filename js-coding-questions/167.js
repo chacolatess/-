0dfd22592c94d439b1c969402d78a046e40f3534
redxffy <150579399+redxffy@users.noreply.github.com>
2024-10-08 19:21:07 +0530

    Update 167.js

--- js-coding-questions/167.js
+++ js-coding-questions/167.js
@@ -8,7 +8,7 @@ function getIntersection(arr1, arr2) {
   // your code here
   const newArr = arr1.filter(it => arr2.includes(it));
   return [...new Set(newArr)];
-} 
+  }
 The getIntersection function finds the common elements between two arrays, arr1 and arr2. Here's a breakdown of how it works:
 
 Filtering: The filter method is applied to arr1. For each element (it) in arr1, it checks if that element is included in arr2 using the includes method. This creates a new array (newArr) that contains only the elements found in both arr1 and arr2.
