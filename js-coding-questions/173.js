05f1c711e3b79797660f2b06184cbfd631fc3b8d
redxffy <150579399+redxffy@users.noreply.github.com>
2024-10-08 12:21:32 +0530

    Create 173.js

---
+++ js-coding-questions/173.js
@@ -0,0 +1,26 @@
+const numeric = (string) => !isNan(parseFloat(string)) &&  isFinite(Number(string));
+function uncompress(str){
+  const stack = [];
+  for (const char of str){
+    if(char !== ')'){
+      stack.push(char);
+    }
+    else{
+      let word ='';
+      let count =''
+      while(stack.length && stack[stack.length-1] !=='('){
+    
+      word= stack.pop() + word;
+      }
+      stack.pop();
+      
+    }
+    while(stack.length && isNumeric(stack[stack.length-1)){
+    
+    count = stack.pop()+ count;
+    }
+    stack.push(word.repeat(Number(count)));
+  }}
+return stack.join('');
+}
+
