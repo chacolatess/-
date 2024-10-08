d3e81e0886a7f3236115577f64948ff8e3b38cf5
redxffy <150579399+redxffy@users.noreply.github.com>
2024-10-08 13:25:18 +0530

    Create 171.js

---
+++ js-coding-questions/171.js
@@ -0,0 +1,24 @@
+const invoke = (fn,...args) => typeof fn ==="function" && fn(...args);
+const once = (fn) => (...args)=> {
+  invoke(fn,...args);fn=null;
+}
+
+const wrap = port =>{
+  const fascade ={};
+  fascade.postMessage=(message,reply)=>{
+    port.postMessage(
+      {message,reply}
+    )
+  }
+  port.onMessage =({message,reply}=>{
+    invoke(fascade.onMessage,message,once(reply))
+  }
+  return fascade;
+}
+class BetterChannel{
+  constructor(){
+    const {port1,port2}= new SomeChannel();
+    this.port1= wrap(port1);
+    this.port2= wrap(port2);
+  }
+}
