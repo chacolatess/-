4abd4f2183a45f2695acc20193480745ee158e93
redxffy <150579399+redxffy@users.noreply.github.com>
2024-10-09 10:05:26 +0530

    Update 159.js

--- js-coding-questions/159.js
+++ js-coding-questions/159.js
@@ -1,16 +1,15 @@
-function promisify(func){
-  return function(...args){
-    return new Promise(resolve,reject){
-      const callbck =(err,data)=>{
-        if(err){
-          reject(err)
+function promisify(func) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      const callback = (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
         }
-        else{
-          resolve(data)
-        }
-      }
-      func.apply(this,..args,callbck);
-      
-    }
-  }
+      };
+      // Call the original function with args and the callback as the last argument
+      func.apply(this, [...args, callback]);
+    });
+  };
 }
