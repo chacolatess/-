91c4566eefd4e5ef6bb079b4aea8b18be30af99a
redxffy <150579399+redxffy@users.noreply.github.com>
2024-10-09 08:50:52 +0530

    Create 10.js

---
+++ js-coding-questions/10.js
@@ -0,0 +1,22 @@
+/*
+The firstBadVersion function is designed to find the first "bad" version in a sequence of versions using a binary search approach. The function accepts a checking function isBad, which determines if a specific version is bad. It returns a closure that takes a version number and identifies the first bad version in the range from 0 to that version.
+
+*/
+
+function FirstBadVersion(isBad){
+  return (version)=>{
+  let start =0;
+  let end = version ;
+    while(start<=end){
+      let mid = Math.floor((start+end)/2)
+      if(isBad(mid)){
+      end=mid-1;
+        
+      }
+      else{
+        end=mid+1;
+      }
+    }
+    return star<=version ? start:-1;
+    }
+}
