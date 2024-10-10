e4f3c35c96713d29326d5bf83f4c5ad651ea785a
chacolatess <150579399+chacolatess@users.noreply.github.com>
2024-10-10 10:56:36 +0530

    Create 90.js

---
+++ js-coding-questions/90.js
@@ -0,0 +1,8 @@
+function myInstanceof(obj,target){
+  if(obj === null || typeof obj!== 'object') return false;
+  while(obj){
+    if(obj.__proto__=== target.prototype) return true;
+    obj=obj.__proto__
+  }
+  return false;
+}
