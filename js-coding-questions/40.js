dca4bf6aabf67c381b5abaa0535c7e716d1c7495
chacolatess <150579399+chacolatess@users.noreply.github.com>
2024-10-10 09:18:08 +0530

    Create 40.js

---
+++ js-coding-questions/40.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr){
+  for(let i=0;i<arr.length;i++){
+    let iSwaped= false;
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[i]>arr[j]){
+        [arr[i],arr[j]]= [arr[j] , arr[i]];
+        iSwaped= false;
+        
+      }
+      
+    }
+    if(!iSwaped){
+      break;
+    }
+  }
+  
+}
